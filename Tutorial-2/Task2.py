25f90282ae41e7e916107f8b5658f9f2f88f0215
AREPALLI SAI VENKATA ADITYA <68097201+asva21@users.noreply.github.com>
2021-02-04 22:10:23 +0530

    Add files via upload

---
+++ Tutorial-2/Task2.py
@@ -0,0 +1,21 @@
+import numpy as np
+import matplotlib.pyplot as plt
+x= np.array([1,2,3])
+y=np.array([1.2,1.9,3.2])
+
+def values(x,y):
+	a=x.sum()
+	b=y.sum()
+	c=(x*x).sum()
+	d=(y*y).sum()
+	e=len(x)
+	w0=(c*b-a*d)/(c*e-a*a)
+	w1=(b-(e*w0))/a
+	return w1,w0
+slope,intercept=values(x,y)
+final_values=[slope*i+ intercept for i in x]
+
+plt.scatter(x,y)
+plt.plot(x,y,'bo')
+plt.plot(x,final_values,'c')
+plt.show()
